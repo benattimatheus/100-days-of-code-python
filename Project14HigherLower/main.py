745332280d831b5c4931682ad1cc1b0fd5c1df87
Matheus Benatti <matheus.cwb2@gmail.com>
2023-02-09 16:06:14 -0300

    Add files via upload

---
+++ Project14HigherLower/main.py
@@ -0,0 +1,64 @@
+from art import logo, vs
+from game_data import data
+import random
+
+def get_random_account():
+  return random.choice(data)
+
+def format_data(account):
+  name = account['name']
+  description = account['description']
+  country = account['country']
+  return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+  if a_followers > b_followers:
+    return guess == 'a'
+  else:
+    return guess == 'b'
+
+def game():
+  print(logo)
+  score = 0
+  game_is_over = False
+
+  accounta = get_random_account()
+  accountb = get_random_account()
+
+  while game_is_over == False:
+    accounta = accountb
+    accountb = get_random_account()
+
+    while accounta == accountb:    
+      accountb = get_random_account()
+      
+    print(f"Compare A: {format_data(accounta)}")
+    print(vs)
+    print(f"Against B: {format_data(accountb)}")
+    guess = input("\nWho was more followers? Type 'A' or 'B': ")
+    a_follower_count = accounta["follower_count"]
+    b_follower_count = accountb["follower_count"]
+    is_right = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_right:
+      score += 1
+      print(f"You're right! Current score: {score}\n")
+    else:
+      game_is_over = True 
+      print(f"Sorry, that's wrong. Final score {score}")
+
+game()
+#ver tamanho do dicionario data = 50
+#escolher 2 aleatórios inicialmente
+#fazer A vs B
+#manter o personagem B
+#quem está em B vai pra A
+#B recebe novo personagem
+
+#Compare A: nome, keys
+#art VS
+#Against B: nome, keys
+#Who was more followers? Type 'A' or 'B': 
+
+#Se acertar printar "You're right! Current score: x"
+#Se perder printar "Sorry, that's wrong. Final score: x"
